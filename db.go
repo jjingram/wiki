2b42b32add277febbebafeb238296521a76b192a
Jarrod Jeffrey Ingram <jarrod.ingram@rungopher.com>
2018-05-14 03:31:50 +1000

    POST now has actual functionality

--- db.go
+++ db.go
@@ -2,18 +2,21 @@ package main
 
 import (
 	"database/sql"
+
+	"github.com/lib/pq"
 )
 
 type Page struct {
 	Id    int
 	Uri   string
-	Title []byte
-	Tags  StringSlice
-	Body  []byte
+	Title string
+	Tags  []string
+	Body  string
 }
 
 type DataAccessLayer interface {
 	GetPage(string) (Page, error)
+	PostPage(page Page) (sql.Result, error)
 }
 
 type DAL struct {
@@ -22,7 +25,12 @@ type DAL struct {
 
 func (dal DAL) GetPage(uri string) (Page, error) {
 	var page Page
-	err := dal.db.QueryRow("select id, uri, title, tags, body from page where uri = $1",
-		title).Scan(&page.Id, &page.Uri, &page.Title, &page.Tags, &page.Body)
+	err := dal.db.QueryRow("select id, uri, title, tags, body from page where uri = $1", uri).Scan(
+		&page.Id, &page.Uri, &page.Title, pq.Array(&page.Tags), &page.Body)
 	return page, err
+}
+
+func (dal DAL) PostPage(page Page) (sql.Result, error) {
+	return dal.db.Exec("insert into page (uri, title, tags, body) values ($1, $2, $3, $4)",
+		page.Uri, page.Title, pq.Array(page.Tags), page.Body)
 }
--- handler.go
+++ handler.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"database/sql"
 	"fmt"
 	"html/template"
-	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 	"unicode"
 
 	"github.com/microcosm-cc/bluemonday"
@@ -46,9 +47,9 @@ func (h *Handler) doGet(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		title := bluemonday.UGCPolicy().SanitizeBytes(page.Title)
+		title := bluemonday.UGCPolicy().SanitizeBytes([]byte(page.Title))
 
-		unsafe := blackfriday.Run(page.Body)
+		unsafe := blackfriday.Run([]byte(page.Body))
 		body := bluemonday.UGCPolicy().SanitizeBytes(unsafe)
 
 		t, err := template.New("layout").ParseFiles("layout.html")
@@ -69,7 +70,7 @@ func (h *Handler) doGet(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			log.Println(err)
 			w.WriteHeader(http.StatusInternalServerError)
-			fmt.Fprintf(w, http.StatusText(http.StatusInternalServerError))
+			fmt.Fprint(w, http.StatusText(http.StatusInternalServerError))
 			return
 		}
 	} else if query != "" {
@@ -82,29 +83,77 @@ func (h *Handler) doPost(w http.ResponseWriter, r *http.Request) {
 	body := r.Body
 	defer body.Close()
 
-	input, err := ioutil.ReadAll(body)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, http.StatusText(http.StatusInternalServerError))
+	headers := make(map[string][]string)
+	scanner := bufio.NewScanner(body)
+	for scanner.Scan() {
+		txt := scanner.Text()
+
+		if txt == "" {
+			break
+		}
+
+		header := strings.Split(txt, ":")
+
+		if len(header) != 2 {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprint(w, http.StatusText(http.StatusBadRequest))
+			return
+		}
+
+		key := strings.ToLower(strings.Trim(header[0], " \t"))
+		val := strings.ToLower(strings.Trim(header[1], " \t"))
+
+		_, ok := headers[key]
+		if !ok {
+			headers[key] = make([]string, 0)
+		}
+		headers[key] = append(headers[key], val)
+	}
+
+	if headers["title"] == nil || headers["tag"] == nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, http.StatusText(http.StatusBadRequest))
 		return
 	}
-	fmt.Fprint(w, input)
+
+	var (
+		page Page
+		err  error
+	)
+	page.Title = headers["title"][0]
+	page.Uri = hyphenate(page.Title)
+	page.Tags = headers["tag"]
+
+	for scanner.Scan() {
+		page.Body += scanner.Text() + "\n"
+	}
+
+	_, err = h.dal.PostPage(page)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, http.StatusText(http.StatusInternalServerError))
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprint(w, http.StatusText(http.StatusCreated))
 }
 
 func hyphenate(text string) string {
-	var anchorName []rune
+	var hyphenated []rune
 	var hyphen = false
 	for _, r := range text {
 		switch {
 		case unicode.IsLetter(r) || unicode.IsNumber(r):
-			if hyphen && len(anchorName) > 0 {
-				anchorName = append(anchorName, '-')
+			if hyphen && len(hyphenated) > 0 {
+				hyphenated = append(hyphenated, '-')
 			}
 			hyphen = false
-			anchorName = append(anchorName, unicode.ToLower(r))
+			hyphenated = append(hyphenated, unicode.ToLower(r))
 		default:
 			hyphen = true
 		}
 	}
-	return string(anchorName)
+	return string(hyphenated)
 }
